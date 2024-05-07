73e49d91eb4f8b39e4d79de99d4e74f5ba7dd621
parastou <jalilian.parastou91@gmail.com>
2024-05-07 12:14:42 +0330

    home component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/home/home';
 
 function App() {
   return (
     <div className="App">
-    
+    <Home/>
     </div>
   );
 }
