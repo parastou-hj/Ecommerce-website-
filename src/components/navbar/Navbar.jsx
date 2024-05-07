7033e29dec55a7e625a127bffac8fb97734f0468
parastou <jalilian.parastou91@gmail.com>
2024-05-07 17:19:09 +0330

    navbar component added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,14 +2,29 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='navbar d-flex'>
-        <div className="logo"></div>
-        <div className="menu">
-            <div className="menu-item"></div>
+    <nav className='navbar '>
+      <div className="top d-flex justify-content-between ">
+        <div className="left d-flex bg-primary">
+          <div className="logo text-primary">PariSha</div>
+          <div className="search">
+            <input type="text" name="" id="" />
+          </div>
         </div>
-        <div className="cart"></div>
-      
-    </div>
+        <div className="center bg-danger">
+          <ul className='menu'>
+            <li><a href="">Home</a></li>
+            <li><a href="">Women</a></li>
+            <li><a href="">Men</a></li>
+            <li><a href="">Children</a></li>
+          </ul>
+        </div>
+        <div className="right bg-success">
+          <div className="login">login/signup</div>
+          <div className="cart">Cart</div>
+        </div>
+      </div>
+      <div className="bottom bg-warning">bye</div>
+    </nav>
   )
 }
 
