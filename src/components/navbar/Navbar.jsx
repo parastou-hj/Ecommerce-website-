ea7cf7c9cbc91a325a8e032b72b1ab38965d76e9
parastou <jalilian.parastou91@gmail.com>
2024-05-07 12:58:17 +0330

    navbar component added

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar d-flex'>
+        <div className="logo"></div>
+        <div className="menu">
+            <div className="menu-item"></div>
+        </div>
+        <div className="cart"></div>
+      
+    </div>
+  )
+}
+
+export default Navbar
