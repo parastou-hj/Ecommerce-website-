1940e9b5f0ec754405e70185c3ca26c7561dd5d7
parastou <jalilian.parastou91@gmail.com>
2024-05-14 12:09:35 +0330

    product-category added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import 'bootstrap/dist/js/bootstrap.js'
 import HeaderNavbar from './components/navbar/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './components/Product/Product';
+import ProductCategory from './components/ProductCategory/ProductCategory';
+import banner_women from './components/assets/banner_women.png'
+import banner_mens from './components/assets/banner_mens.png'
+import banner_kids from './components/assets/banner_kids.png'
 
 
 function App() {
@@ -16,6 +20,9 @@ function App() {
       </header>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path='/women' element={<ProductCategory category='women' banner={banner_women} />} />
+        <Route path='/men' element={<ProductCategory category='men'  banner={banner_mens} />} />
+        <Route path='/kid' element={<ProductCategory category='kid'  banner={banner_kids} />} />
         <Route path="/product" element={<Product/>}>
         <Route path="/product:productId" element={<Product/>}/>
         </Route>
